2fe82d8eaeb337a226c1ee82aa7e4ccd891a1141
vinogradova311 <anny.amelie@gmail.com>
2022-07-14 20:08:10 +0300

    Changed code

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,18 @@
 public class Main {
     public static void main(String[] args) {
         CreditPaymentService service = new CreditPaymentService();
-        double mYear1 = 9.99; // годовая процентная ставка
-        double m1 = (mYear1 / 12) / 100; // процентная ставка в месяц
-        int n1 = 12; // кол-во ежемесячных платежей
-        int amount1 = 1000000; // сумма кредита
+        double mYear = 9.99; // годовая процентная ставка
+        double m = (mYear / 12) / 100; // процентная ставка в месяц
+        int n = 12; // кол-во ежемесячных платежей
+        int amount = 1000000; // сумма кредита
 
-        int monthlyPayment = service.calculate((double) m1, n1, amount1);
-        System.out.println(monthlyPayment);
+        int monthlyPayment1 = service.calculate((double) m, n, amount);
+        System.out.println(monthlyPayment1);
 
-
-        double mYear2 = 9.99; // годовая процентная ставка
-        double m2 = ((mYear2 / 12) / 100); // процентная ставка в месяц
-        int n2 = 24; // кол-во ежемесячных платежей
-        int amount2 = 1000000; // сумма кредита
-
-        int monthlyPayment2 = service.calculate2((double) m2, n2, amount2);
+        int monthlyPayment2 = service.calculate((double) m, n * 2, amount);
         System.out.println(monthlyPayment2);
 
-        double mYear3 = 9.99; // годовая процентная ставка
-        double m3 = (mYear3 / 12) / 100; // процентная ставка в месяц
-        int n3 = 36; // кол-во ежемесячных платежей
-        int amount3 = 1000000; // сумма кредита
-
-        int monthlyPayment3 = service.calculate3((double) m3, n3, amount3);
+        int monthlyPayment3 = service.calculate((double) m, n * 3, amount);
         System.out.println(monthlyPayment3);
     }
 }
